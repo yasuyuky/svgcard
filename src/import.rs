53e432b1a13f90a06c88c4340d7b585c5229db3b
yasuyuky <yasuyuki.ymd@gmail.com>
2021-04-18 09:59:42 +0900

    Add import module

---
+++ src/import.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use std::fs::File;
+use std::io::{BufReader, Write};
+use std::path::Path;
+use xml::{reader as r, writer as w};
+
+pub fn import_svg<W: Write>(writer: &mut w::EventWriter<W>, path: &Path) -> Result<()> {
+    let file = File::open(path).unwrap();
+    let file = BufReader::new(file);
+
+    let parser = r::EventReader::new(file);
+    let mut inside_svg = false;
+    for e in parser {
+        match &e {
+            Ok(r::XmlEvent::StartDocument { .. }) => {}
+            Ok(r::XmlEvent::EndDocument { .. }) => {}
+            Ok(r::XmlEvent::StartElement { name, .. }) => {
+                if name.local_name == "svg" {
+                    inside_svg = true;
+                    let g: w::XmlEvent = w::XmlEvent::start_element("g").into();
+                    writer.write(g)?;
+                } else if inside_svg {
+                    writer.write(e.unwrap().as_writer_event().unwrap())?;
+                }
+            }
+            Ok(r::XmlEvent::EndElement { .. }) => {
+                let e: w::XmlEvent = w::XmlEvent::end_element().into();
+                writer.write(e)?;
+            }
+            Ok(ev) => {
+                writer.write(ev.as_writer_event().unwrap())?;
+            }
+            _ => {}
+        }
+    }
+    Ok(())
+}
