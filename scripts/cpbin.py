573071b5bd9d8d8692f37fd8eee06da782b6686d
yasuyuky <yasuyuki.ymd@gmail.com>
2023-09-06 17:49:05 +0900

    Add cpbin.py script to copy svgcard binary to corresponding platform directories if it exists, or print a message if it doesn't exist

---
+++ scripts/cpbin.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+
+platforms = {
+    "aarch64-unknown-linux-musl": "linux/arm64",
+    "x86_64-unknown-linux-musl": "linux/amd64"
+}
+
+for triple, platform in platforms:
+    print(triple, platform)
+    if os.path.isfile(f"target/{triple}/release/svgcard"):
+        os.makedirs(f"output/{platform}", exist_ok=True)
+        shutil.copy(f"target/{triple}/release/svgcard", f"output/{platform}/svgcard")
+        print(f"target/{triple}/release/svgcard -> output/{platform}/svgcard")
+    else:
+        print(f"target/{triple}/release/svgcard does not exist")
